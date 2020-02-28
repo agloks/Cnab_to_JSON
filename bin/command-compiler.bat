c036855c2c084ac2bcbba320cd5662069e1c9beb
Agloks <perdi9090@yahoo.com>
2020-02-28 19:01:40 -0300

    need readjust variables on read config

--- bin/command-compiler.bat
+++ bin/command-compiler.bat
@@ -1,3 +1,7 @@
+echo "COMPILANDO"
+
 cl /c /I .. /EHsc ..\source\date_from_cnab.cpp
+cl /c /I .. /EHsc ..\source\read_from_config.cpp
 lib .\date_from_cnab.obj
-cl /I .. /EHsc ..\source\main.cpp /link .\date_from_cnab.lib
+lib .\read_from_config.obj
+cl /I .. /EHsc ..\source\main.cpp /link .\date_from_cnab.lib .\read_from_config.lib
