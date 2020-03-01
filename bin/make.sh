c10b1c083e323ba0dce1f4fc1196a2e35bda148e
Agloks <perdi9090@yahoo.com>
2020-03-01 12:37:24 -0300

    make.sh modified

--- bin/make.sh
+++ bin/make.sh
@@ -3,6 +3,8 @@
 echo "starting..."
 
 g++ -c ../source/date_from_cnab.cpp -I .. -o libdate_from_cnab.o
+g++ -c ../source/read_from_config.cpp -I .. -o libread_from_config.o
 ar rcs libdate_from_cnab.a libdate_from_cnab.o
+ar rcs libread_from_config.a libread_from_config.o
 g++ -c ../source/main.cpp -I .. -o main.o
-g++ -o main.exe main.o -I . -L . -l date_from_cnab
+g++ -o main.exe main.o -I . -L . -l date_from_cnab -l read_from_config
